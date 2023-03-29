53ac6f373c9d9109188c4d2e0794f3c970b3d161
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-03-29 01:22:37 +0100

    puppet-lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-ll command to kill bashscript file killmenow
-exec { 'pkill':
-  command => 'pkill -9 -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+{ 'killmenow':
+  command => 'pkill killmenow',
+  path    => ['/bin', '/usr/bin', '/usr/local/bin'], # Optional: set the path for the 'pkill' command
+  onlyif  => 'pgrep killmenow', # Optional: ensure that the process exists before trying to kill it
+  logoutput => true, # Optional: log the output of the command
 }
+
